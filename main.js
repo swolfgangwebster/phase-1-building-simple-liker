47119a8150da4aafef1a144a6a8bc3d6788620fa
Shawn Webster <swolfgangwebster@gmail.com>
2023-05-07 17:21:29 -0500

    ??

--- main.js
+++ main.js
@@ -3,7 +3,30 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+all = document.querySelectorAll('.like-glyph')
 
+all.forEach(a => {
+//document.querySelector(".like-glyph")
+a.addEventListener('click', (e) => {
+    mimicServerCall().then(() => {
+      if (e.target.innerHTML == EMPTY_HEART) {
+      e.target.innerHTML = FULL_HEART
+      e.target.classList.add("activated-heart")
+      }
+      else { 
+        e.target.innerHTML = EMPTY_HEART
+        e.target.classList.remove("activated-heart")
+
+      }
+    })
+    .catch((e) => {
+      document.querySelector("#modal").classList.remove("hidden")
+      setTimeout(document.querySelector("#modal").classList.add("hidden"), 3000)
+      console.log("???")
+    })
+
+})
+})
 
 
 
